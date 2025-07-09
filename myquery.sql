b4a7e1b91b7d57aae05a6c696aaa25a08674922d
ainteraminense <99498988+ainteraminense@users.noreply.github.com>
2025-07-08 21:40:52 -0400

    Get Country with highest amount of debt

--- myquery.sql
+++ myquery.sql
@@ -1,2 +1,8 @@
 SELECT COUNT(DISTINCT country_code) AS total_distinct_countries
 FROM international_debt;
+
+-- Country with highest amount of debt
+SELECT country_name, debt AS total_debt
+FROM international_debt
+ORDER BY total_debt DESC
+LIMIT 1; 
